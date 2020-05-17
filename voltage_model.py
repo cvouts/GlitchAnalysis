3a91e320be48304773423c14df6d64fa6079bc22
cvoutsadakis <christos.vouts@gmail.com>
2020-05-17 20:54:15 +0300

    do some cleanup

--- voltage_model.py
+++ voltage_model.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import pickle
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split, GridSearchCV
+import matplotlib.pyplot as plt
 from warnings import simplefilter
 simplefilter(action="ignore", category=FutureWarning)
 
@@ -14,46 +17,69 @@ output_list = []
 for i in range(1, NUMBER_OF_VOLTAGE_VALUES+1):
     output_list.append("V3-" + str(i))
 
+plt.axis([1, 200, 0.95, 1.12])
+time_axis = []
+for i in range(1, 201):
+    time_axis.append(i)
+
 x = data_input[["C", "T1", "T2", "DIS", "HDIST", "V3-0"]]
 y = data_output[output_list]
 
 params = {"n_neighbors": [2, 3, 4, 5, 6, 7, 8, 9, 10]}
 knn = KNeighborsRegressor()
 
-test_size = 0.1
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size)
+accuracy_sum = 0
+best_accuracy = 0
 
-neighbor_selection_model = GridSearchCV(knn, params, cv=5)  # finding the best number of neighbors
-neighbor_selection_model.fit(x_train, y_train)
-neighbor_number = sum(neighbor_selection_model.best_params_.values())
-model = KNeighborsRegressor(n_neighbors=neighbor_number)
-model.fit(x_train, y_train)
+for it in range(10):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-# finding model accuracy using .score
-print(model.score(x_test, y_test))
+    neighbor_selection_model = GridSearchCV(knn, params, cv=5)  # finding the best number of neighbors
+    neighbor_selection_model.fit(x_train, y_train)
+    neighbor_number = sum(neighbor_selection_model.best_params_.values())
+    model = KNeighborsRegressor(n_neighbors=neighbor_number)
+    model.fit(x_train, y_train)
 
-prediction = model.predict(x_test)
+    train_prediction = model.predict(x_train)
+    test_prediction = model.predict(x_test)
 
-# finding model accuracy manually
-correct_predictions = 0
-below50 = 0
-above80 = 0
-test_size = int(NUMBER_OF_DATA * test_size)
-voltage_per_data = NUMBER_OF_VOLTAGE_VALUES
-for i in range(0, test_size):   # for each piece of data in the test dataset
-    sum_of_previous_predictions = correct_predictions
-    for j in range(0, voltage_per_data):  # for each voltage value in a test dataset piece
-        if y_test.iat[i, j] == prediction[i, j].round(4):  # if actual value == predicted value
-            correct_predictions += 1
+    # train_error = mean_squared_error(y_train, train_prediction)
+    # test_error = mean_squared_error(y_test, test_prediction)
+    # print("train error:", train_error, "test error:", test_error)
 
-    print("For instance", i+1, "the accuracy was ",
-          round((correct_predictions-sum_of_previous_predictions)*100/voltage_per_data, 2), "%")  # rule of 3
+    # if it == 0:
+    #     with open("Models/voltage_model", "wb") as f:
+    #         pickle.dump(model, f)
+    #     model_test_data_x = open("Models/model_test_data_x.txt", "w")
+    #     model_test_data_y = open("Models/model_test_data_y.txt", "w")
+    #     model_test_data_x.write(x_test.to_string())
+    #     model_test_data_y.write(y_test.to_string())
+    #     model_test_data_x.close()
+    #     model_test_data_y.close()
+    # else:
+    #     break
 
-    if round((correct_predictions-sum_of_previous_predictions)*100/voltage_per_data, 2) < 50:
-        below50 += 1
-    elif round((correct_predictions-sum_of_previous_predictions)*100/voltage_per_data, 2) >= 80:
-        above80 += 1
+    prediction = model.predict(x_test)
 
-accuracy = round((correct_predictions * 100 / (voltage_per_data*test_size)), 2)  # rule of 3
-print(accuracy, "% accuracy, with the number of instances equal or above 80% at", above80,
-      "/1078 and below 50% at", below50, "/1078")
+    # finding model accuracy manually
+    correct_predictions = 0
+    for i in range(0, y_test.shape[0]):   # for each piece of data in the test dataset
+        sum_of_previous_predictions = correct_predictions
+        for j in range(0, y_test.shape[1]):  # for each voltage value in a test dataset piece
+
+            if y_test.iat[i, j] == prediction[i, j].round(4):  # if actual value == predicted value
+                correct_predictions += 1
+
+        plt.plot(time_axis, y_test.iloc[i, :], "b-")
+        plt.plot(time_axis, test_prediction[i, :], "r-")
+        title = "instance " + str(i + 1)
+        plt.title(title)
+        plt.show()
+
+    accuracy = round((correct_predictions * 100 / y_test.size), 2)  # rule of 3
+    print(accuracy, "% accuracy")
+
+    accuracy_sum += accuracy
+    if accuracy > best_accuracy:
+        best_accuracy = accuracy
+print("average accuracy is", round((accuracy_sum/10), 2), "%, best accuracy is", best_accuracy, "%")
