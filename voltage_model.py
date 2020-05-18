81d6670f502bb2a6fa1bab963ce47b25c672a44e
cvoutsadakis <christos.vouts@gmail.com>
2020-05-18 22:52:09 +0300

    change the algorithm to Random Forest, use standardised x and drop the HDIST column

--- voltage_model.py
+++ voltage_model.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import numpy as np
 import pickle
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from warnings import simplefilter
 simplefilter(action="ignore", category=FutureWarning)
@@ -24,31 +26,29 @@ for i in range(1, 201):
 x = data_input[["C", "T1", "T2", "DIS", "HDIST"]]
 y = data_output[output_list]
 
-params = {"n_neighbors": [2, 3, 4, 5, 6, 7, 8, 9, 10]}
-knn = KNeighborsRegressor()
-
 accuracy_sum = 0
 best_accuracy = 0
-
+model = RandomForestRegressor()
 for it in range(10):
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-    # sc = StandardScaler()
-    # x_train = sc.fit_transform(x_train)
-    # x_test = sc.transform(x_test)
+    sc = StandardScaler()
+    x_train = sc.fit_transform(x_train)
+    x_test = sc.transform(x_test)
 
-    neighbor_selection_model = GridSearchCV(knn, params, cv=5)  # finding the best number of neighbors
-    neighbor_selection_model.fit(x_train, y_train)
-    neighbor_number = sum(neighbor_selection_model.best_params_.values())
-    model = KNeighborsRegressor(n_neighbors=neighbor_number)
     model.fit(x_train, y_train)
 
-    train_prediction = model.predict(x_train)
+    # dropping HDIST
+    x_train = np.delete(x_train, 4, axis=1)
+    x_test = np.delete(x_test, 4, axis=1)
+
+    model.fit(x_train, y_train)
     test_prediction = model.predict(x_test)
 
+    train_prediction = model.predict(x_train)
     train_error = mean_squared_error(y_train, train_prediction)
     test_error = mean_squared_error(y_test, test_prediction)
-    print("Standardised train error", train_error, "test error", test_error)
+    print("dropped train error", train_error, "test error", test_error)
 
     # if it == 0:
     #     with open("Models/voltage_model", "wb") as f:
@@ -62,6 +62,7 @@ for it in range(10):
 
     # finding model accuracy manually
     correct_predictions = 0
+    flag = 0
     for i in range(0, y_test.shape[0]):   # for each piece of data in the test dataset
         sum_of_previous_predictions = correct_predictions
         for j in range(0, y_test.shape[1]):  # for each voltage value in a test dataset piece
@@ -69,12 +70,18 @@ for it in range(10):
             if y_test.iat[i, j] == test_prediction[i, j].round(4):  # if actual value == predicted value
                 correct_predictions += 1
 
-        plt.axis([1, 200, 0.95, 1.12])
-        plt.plot(time_axis, y_test.iloc[i, :], "b-")
-        plt.plot(time_axis, test_prediction[i, :], "r-")
-        title = "instance " + str(i + 1)
-        plt.title(title)
-        plt.show()
+            if y_test.iat[i, j] < 1:
+                flag = 1
+
+        # plot only for instances that actually fall below 1V
+        if flag == 1:
+            flag = 0
+            plt.axis([1, 200, 0.95, 1.12])
+            plt.plot(time_axis, y_test.iloc[i, :], "b-")
+            plt.plot(time_axis, test_prediction[i, :], "r-")
+            title = "instance " + str(i + 1)
+            plt.title(title)
+            plt.show()
 
     accuracy = round((correct_predictions * 100 / y_test.size), 2)  # rule of 3
     print(accuracy, "% accuracy")
