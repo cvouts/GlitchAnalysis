ccd96c7216fc082b3eb7457fcb57f2930a68ee93
cvoutsadakis <christos.vouts@gmail.com>
2020-05-19 02:10:53 +0300

    only keep 20 values from 200, because 200 are not necessary and skew the results instead. The mean squared error currently is at about 3-10

--- current_model.py
+++ current_model.py
@@ -1,48 +1,39 @@
+from os import path
 import pandas as pd
 import pickle
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
+from sklearn.multioutput import  MultiOutputRegressor
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-
 from warnings import simplefilter
 simplefilter(action="ignore", category=FutureWarning)
-
-NUMBER_OF_DATA = 10780
-NUMBER_OF_CURRENT_VALUES = 200
 
 data_input = pd.read_csv("Data/CSV/Currents/input_currents.csv", sep=",")
 data_output = pd.read_csv("Data/CSV/Currents/output_currents.csv", sep=",")
 
 output_list = []
-for i in range(1, NUMBER_OF_CURRENT_VALUES+1):
+for i in range(1, 20+1):
     output_list.append("I(V6)-" + str(i))
 
-x = data_input[["C", "T1", "T2", "DIS", "HDIST"]]
+x = data_input[["C", "T1", "T2", "DIS"]]
 y = data_output[output_list]
 
-plt.axis([1, 200, -75, 125])
+plt.axis([1, 20, -75, 125])
 time_axis = []
-for i in range(1, 201):
+for i in range(1, 21):
     time_axis.append(i)
 
-accuracy_sum = 0
-best_accuracy = 0
+model = MultiOutputRegressor(RandomForestRegressor(n_estimators=100, random_state=0))
 
-model = RandomForestRegressor()
-for it in range(10):
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+best_test_error = 200
+for it in range(5):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
 
     sc = StandardScaler()
     x_train = sc.fit_transform(x_train)
     x_test = sc.transform(x_test)
-
-    x_standardized = StandardScaler().fit_transform(x)
-    pca = PCA()
-    pca.fit(x_standardized)
-    print("PCA variance ratios for standardized x:", pca.explained_variance_ratio_)
 
     model.fit(x_train, y_train)
 
@@ -51,31 +42,56 @@ for it in range(10):
 
     train_error = mean_squared_error(y_train, train_prediction)
     test_error = mean_squared_error(y_test, test_prediction)
-    print("Standardised x_train and x_test\nRandomForest: train error", train_error, "test error", test_error)
+    print(it+1, "Forest train error", train_error.round(2), "test error", test_error.round(2))
 
+    if best_test_error > test_error:
+        best_test_error = test_error
+        print("best error now is", best_test_error.round(2))
+        best_x_train = x_train
+        best_x_test = x_test
+        best_y_train = y_train
+        best_y_test = y_test
 
-    # finding model accuracy manually
-    correct_predictions = 0
-    for i in range(0, y_test.shape[0]):  # for each piece of data in the test dataset
+    if best_test_error < 5:
+        break
 
-        sum_of_previous_predictions = correct_predictions
-        for j in range(0, y_test.shape[1]):  # for each voltage value in a test dataset piece
-            if y_test.iat[i, j].round(4) == test_prediction[i, j].round(4):  # if actual value == predicted value
-                correct_predictions += 1
+print("best error was", best_test_error.round(2))
+x_train = best_x_train
+x_test = best_x_test
+y_train = best_y_train
+y_test = best_y_test
 
-        plt.plot(time_axis, y_test.iloc[i, :], "b-")
-        plt.plot(time_axis, test_prediction[i, :], "r-")
-        title = "instance " + str(i+1)
-        plt.title(title)
-        plt.show()
+model.fit(x_train, y_train)
+test_prediction = model.predict(x_test)
 
-    accuracy = round((correct_predictions * 100 / y_test.size), 2)  # rule of 3
-    print(correct_predictions, "--", accuracy, "% accuracy")
+# finding model accuracy manually
+less_than_2 = 0
+wrong_for_more_than_10_flag = 0
+correct_predictions_flag = 0
+more_than_10 = 0
+less_than_10 = 0
+for i in range(0, y_test.shape[0]):  # for each piece of data in the test dataset
 
-    accuracy_sum += accuracy
-    if accuracy > best_accuracy:
-        best_accuracy = accuracy
-print("average accuracy is", round((accuracy_sum / 10), 2), "%, best accuracy is", best_accuracy, "%")
+    for j in range(0, y_test.shape[1]):  # for each voltage value in a test dataset piece
+        if abs(y_test.iat[i, j].round(4)-test_prediction[i, j].round(4)) < 2:  # comparing actual and predicted values
+            correct_predictions_flag = 1
+        elif abs(y_test.iat[i, j].round(4)-test_prediction[i, j].round(4)) > 10:
+            wrong_for_more_than_10_flag = 1
 
+    if wrong_for_more_than_10_flag == 1:
+        wrong_for_more_than_10_flag = 0
+        more_than_10 += 1
+    else:
+        less_than_10 += 1
+        if correct_predictions_flag == 1:
+            correct_predictions_flag = 0
+            less_than_2 += 1
 
+    # plt.plot(time_axis, y_test.iloc[i, :], "b-")
+    # plt.plot(time_axis, test_prediction[i, :], "r-")
+    # title = "instance " + str(i+1)
+    # plt.title(title)
+    # plt.show()
 
+print("difference of more than 10:", more_than_10, "\ndifference of less than 10:", less_than_10,
+      "\ndifference of less than 2:", less_than_2)
