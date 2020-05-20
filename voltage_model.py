a90aa7ebfc46661a90346d9f8ca70e920bb2d8d0
cvoutsadakis <christos.vouts@gmail.com>
2020-05-21 02:33:59 +0300

    remove DIS column, refactor closer to the current model code

--- voltage_model.py
+++ voltage_model.py
@@ -3,7 +3,7 @@ import pickle
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 import matplotlib.pyplot as plt
 from warnings import simplefilter
 simplefilter(action="ignore", category=FutureWarning)
@@ -22,28 +22,36 @@ time_axis = []
 for i in range(1, 201):
     time_axis.append(i)
 
-x = data_input[["C", "T1", "T2", "DIS"]]
+x = data_input[["C", "T1", "T2", "HDIST"]]
 y = data_output[output_list]
 
 accuracy_sum = 0
 best_accuracy = 0
 model = RandomForestRegressor()
-for it in range(10):
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
+best_test_error = 200
+
+# for it in range(10):
+#     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+kf = KFold(n_splits=5, shuffle=True)
+for train_index, test_index in kf.split(x, y):
+    x_train = x.iloc[train_index]
+    y_train = y.iloc[train_index]
+    x_test = x.iloc[test_index]
+    y_test = y.iloc[test_index]
+
+    # stardardize x
     sc = StandardScaler()
     x_train = sc.fit_transform(x_train)
     x_test = sc.transform(x_test)
 
-    model.fit(x_train, y_train)
-
+    # compare MSE for x_train and for x_test
     model.fit(x_train, y_train)
     test_prediction = model.predict(x_test)
-
     train_prediction = model.predict(x_train)
     train_error = mean_squared_error(y_train, train_prediction)
     test_error = mean_squared_error(y_test, test_prediction)
-    print("dropped train error", train_error, "test error", test_error)
+    print("train error", train_error, "test error", test_error)
 
     # if it == 0:
     #     with open("Models/voltage_model", "wb") as f:
@@ -55,33 +63,46 @@ for it in range(10):
     #     model_test_data_x.close()
     #     model_test_data_y.close()
 
-    # finding model accuracy manually
-    correct_predictions = 0
-    flag = 0
-    for i in range(0, y_test.shape[0]):   # for each piece of data in the test dataset
-        sum_of_previous_predictions = correct_predictions
-        for j in range(0, y_test.shape[1]):  # for each voltage value in a test dataset piece
+    if best_test_error > test_error:
+        best_test_error = test_error
+        print("best error now is", best_test_error.round(2))
+        best_x_train = x_train
+        best_x_test = x_test
+        best_y_train = y_train
+        best_y_test = y_test
 
-            if y_test.iat[i, j] == test_prediction[i, j].round(4):  # if actual value == predicted value
-                correct_predictions += 1
+print("best error was", best_test_error.round(2))
+x_train = best_x_train
+x_test = best_x_test
+y_train = best_y_train
+y_test = best_y_test
 
-            if y_test.iat[i, j] < 1:
-                flag = 1
+model.fit(x_train, y_train)
+test_prediction = model.predict(x_test)
 
-        # plot only for instances that actually fall below 1V
-        if flag == 1:
-            flag = 0
-            plt.axis([1, 200, 0.95, 1.12])
-            plt.plot(time_axis, y_test.iloc[i, :], "b-")
-            plt.plot(time_axis, test_prediction[i, :], "r-")
-            title = "instance " + str(i + 1)
-            plt.title(title)
-            plt.show()
+flag = 0
+for i in range(0, y_test.shape[0]):   # for each piece of data in the test dataset
+    list_of_differences = []
+    for j in range(0, y_test.shape[1]):  # for each voltage value in a test dataset piece
 
-    accuracy = round((correct_predictions * 100 / y_test.size), 2)  # rule of 3
-    print(accuracy, "% accuracy")
+        difference = abs(y_test.iat[i, j].round(4) - test_prediction[i, j].round(4))
+        list_of_differences.append(difference)
 
-    accuracy_sum += accuracy
-    if accuracy > best_accuracy:
-        best_accuracy = accuracy
-print("average accuracy is", round((accuracy_sum/10), 2), "%, best accuracy is", best_accuracy, "%")
+        if y_test.iat[i, j] < 1:
+            flag = 1
+
+    max_difference = max(list_of_differences)
+    print(max_difference)
+
+    # plot only for instances that actually fall below 1V
+    if flag == 1:
+        flag = 0
+        plt.axis([1, 200, 0.95, 1.12])
+        actual_line, = plt.plot(time_axis, y_test.iloc[i, :], "b-")
+        predicted_line, = plt.plot(time_axis, test_prediction[i, :], "r-")
+        title = "instance " + str(i + 1)
+        plt.legend((actual_line, predicted_line), ("actual", "predicted"))
+        plt.title(title)
+        plt.xlabel("picosecond")
+        plt.ylabel("volt")
+        plt.show()
