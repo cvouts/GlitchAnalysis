ee48ec5b0d0d266149ca7b67e35ddbe9331d4b79
cvoutsadakis <christos.vouts@gmail.com>
2020-07-27 18:50:06 +0300

    add initial value

--- half_check.py
+++ half_check.py
@@ -4,20 +4,23 @@ import matplotlib.pyplot as plt
 half = pd.read_csv("Data/CSV/Currents/output_currents_half.csv", sep=",")
 whole = pd.read_csv("Data/CSV/Currents/output_currents.csv", sep=",")
 
-time_axis = []
+time_axis = [0]
 for k in range(1, 201):
     time_axis.append(k)
 
-time_axis_half = []
+time_axis_half = [0]
 for k in range(1, 41):
     time_axis_half.append(k/2)
 
 for i in range(0, half.shape[0]):
 
-    print(i)
-    plt.axis([1, 20, -50, 90])
-    line_whole, = plt.plot(time_axis, whole.iloc[i, :], "b-")
+    whole_with_initial = pd.Series([0]).append(whole.iloc[i])
+    half_with_initial = pd.Series([0]).append(half.iloc[i])
 
-    line_half, = plt.plot(time_axis_half, half.iloc[i, :], "r-")
+    print(i)
+    plt.axis([0, 20, -50, 90])
+    line_whole, = plt.plot(time_axis, whole_with_initial, "b-")
+
+    line_half, = plt.plot(time_axis_half, half_with_initial, "m-")
     plt.legend((line_whole, line_half), ("1ps", "0.5ps"))
     plt.show()
