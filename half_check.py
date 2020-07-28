838b61232967cbb4f2c033b06efda1e2f47fe10e
cvoutsadakis <christos.vouts@gmail.com>
2020-07-28 19:12:59 +0300

    change the axes, add labels to them, add a title, improve clarity to the plots themselves, add comments

--- half_check.py
+++ half_check.py
@@ -1,6 +1,11 @@
+# tests whether the current output values with a step of 1 ps and those of a 0.5 ps step are the same
+# or if the 0.5 ps step version picks up on spikes in value missed in the 1 ps step version.
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# the 1 ps step has values up to 200 ps, while the 0.5 ps step has values up to 20 ps
 half = pd.read_csv("Data/CSV/Currents/output_currents_half.csv", sep=",")
 whole = pd.read_csv("Data/CSV/Currents/output_currents.csv", sep=",")
 
@@ -14,13 +19,16 @@ for k in range(1, 41):
 
 for i in range(0, half.shape[0]):
 
+    # add initial 0 values for clarity
     whole_with_initial = pd.Series([0]).append(whole.iloc[i])
     half_with_initial = pd.Series([0]).append(half.iloc[i])
 
-    print(i)
-    plt.axis([0, 20, -50, 90])
-    line_whole, = plt.plot(time_axis, whole_with_initial, "b-")
-
-    line_half, = plt.plot(time_axis_half, half_with_initial, "m-")
+    # these axes provided a zoomed in view of the difference
+    plt.axis([0, 9, -20, 60])
+    line_half, = plt.plot(time_axis_half, half_with_initial, "m-^")
+    line_whole, = plt.plot(time_axis, whole_with_initial, "b-v")
     plt.legend((line_whole, line_half), ("1ps", "0.5ps"))
+    plt.xlabel("Time (ps)")
+    plt.ylabel("Current (uA)")
+    plt.title("Comparing different time steps")
     plt.show()
