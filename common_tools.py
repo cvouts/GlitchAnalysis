ecccf4c63e997212447ffd90b19793c4cfc088c1
cvoutsadakis <christos.vouts@gmail.com>
2020-07-28 22:45:21 +0300

    comment and clean up

--- common_tools.py
+++ common_tools.py
@@ -1,3 +1,6 @@
+# methods used in multiple other files. Most are shared between voltage_model and current_model
+
+
 import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,6 +10,8 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.model_selection import GridSearchCV
 
 
+# creates a scaler with the help of the train subset, uses the scaler on the current test subset.
+# type_of_scaler determines if it was called to save a scaler and if so, what kind of scaler
 def standardize_train_test_data(x_train, x_test, type_of_scaler):
     scaler = StandardScaler()
     standard_x_train = scaler.fit_transform(x_train)
@@ -20,6 +25,7 @@ def standardize_train_test_data(x_train, x_test, type_of_scaler):
         return standard_x_train, standard_x_test
 
 
+# pca analysis of the input
 def get_pca(x_in):
     x_standardized = StandardScaler().fit_transform(x_in)
     pca = PCA()
@@ -28,6 +34,7 @@ def get_pca(x_in):
     print(pd.DataFrame(pca.components_, columns=x_in.columns))
 
 
+# returns the mean squared error of the test data subset. For a current model, rounds to 2 decimals
 def train_test_mean_error(model_kind, model, x_train, x_test, y_train, y_test):
     train_pred = model.predict(x_train)
     test_pred = model.predict(x_test)
@@ -40,22 +47,32 @@ def train_test_mean_error(model_kind, model, x_train, x_test, y_train, y_test):
         test_error = test_error.round(2)
 
     print("train error", train_error, "test error", test_error)
-
     return test_error
 
 
-def real_and_predicted_plots(x_test_values, y_actual, y_prediction, ylabel, axis, mae):
-    plt.axis(axis)
+# plots the actual values and the predicted values on the same graph
+def real_and_predicted_plots(x_test_values, y_actual, y_prediction, ylabel, mae):
     time_axis = [0]
 
+    # different parameters are required for each kind of graph, voltage or current
     if ylabel == "Voltage (V)":
         upper_limit = 201
         y_actual_with_initial = pd.Series([1.1]).append(y_actual)
         y_prediction_with_initial = pd.Series([1.1]).append(y_prediction)
+        mae_text = "Mean Absolute Error: " + str(mae.round(8)) + "V"
+        plt.text(50, 0.96, mae_text)
+        plt.axis([0, 200, 0.95, 1.12])
+        actual_plot_style = "b-"
+        redicted_plot_style = "r-"
     else:
         upper_limit = 41
         y_actual_with_initial = pd.Series([0]).append(y_actual)
         y_prediction_with_initial = pd.Series([0]).append(y_prediction)
+        mae_text = "Mean Absolute Error: " + str(mae.round(2)) + "uA"
+        plt.text(5, -40, mae_text)
+        plt.axis([0, 20, -50, 75])
+        actual_plot_style = "b-D"   # the plots of the currents indicate the actual values
+        redicted_plot_style = "r-o"
 
     for k in range(1, upper_limit):
         if upper_limit == 41:
@@ -63,20 +80,18 @@ def real_and_predicted_plots(x_test_values, y_actual, y_prediction, ylabel, axis
         else:
             time_axis.append(k)
 
-    mae_text = "Mean Absolute Error: " + str(mae.round(2)) + "uA"
-
-    actual_line, = plt.plot(time_axis, y_actual_with_initial, "b-")
-    predicted_line, = plt.plot(time_axis, y_prediction_with_initial, "r-")
+    actual_line, = plt.plot(time_axis, y_actual_with_initial, actual_plot_style, markersize=5)
+    predicted_line, = plt.plot(time_axis, y_prediction_with_initial, redicted_plot_style, markersize=4)
     title = x_test_values
     plt.legend((actual_line, predicted_line), ("actual", "predicted"))
     plt.title(title)
-    plt.text(5, -40, mae_text)
     plt.xlabel("Time (ps)")
     plt.ylabel(ylabel)
-    # plt.figure()
     plt.show()
 
 
+# compares the actual simulation values and the predicted ones. Prints the mean absolute error of each
+# and the average mean absolute error across the entire test subset. Can also be used to show the plots
 def compare_real_and_predicted(x_test, y_test, test_prediction, plot_ylabel):
     average_mae = 0
     less_than_two = 0
@@ -97,10 +112,19 @@ def compare_real_and_predicted(x_test, y_test, test_prediction, plot_ylabel):
         else:
             more_than_five += 1
 
-        if mae < 2:
-            print("less than two")
-            real_and_predicted_plots(format_x_test_string_data(x_test.iloc[i]), y_test_row,
-                                     prediction_row, plot_ylabel, [0, 20, -50, 75], mae)
+        # if mae < 2:
+        #     print("less than two")
+        #     real_and_predicted_plots(format_x_test_string_data(x_test.iloc[i]), y_test_row,
+        #                              prediction_row, plot_ylabel,  mae)
+        # if 2 < mae < 5:
+        #     print("two to five")
+        #     real_and_predicted_plots(format_x_test_string_data(x_test.iloc[i]), y_test_row,
+        #                              prediction_row, plot_ylabel, mae)
+        # if mae > 5:
+        #     print("more than five")
+        #     real_and_predicted_plots(format_x_test_string_data(x_test.iloc[i]), y_test_row,
+        #                              prediction_row, plot_ylabel, mae)
+
 
     print("MAE of less than 2:", round(((less_than_two*100)/y_test.shape[0]), 2), "%\nbetween 2 and 5:",
           round(((two_to_five*100)/y_test.shape[0]), 2), "%\nmore than 5:",
@@ -109,6 +133,8 @@ def compare_real_and_predicted(x_test, y_test, test_prediction, plot_ylabel):
     print("average mean absolute error:", average_mae)
 
 
+# grid search for the hyperparameters of the model. Repeated fitting of
+# the standardized input across the grid of possible hyperparameter values provided
 def grid_search(estimator, grid, x, y):
     sc = StandardScaler()
     x = sc.fit_transform(x)
@@ -121,6 +147,7 @@ def grid_search(estimator, grid, x, y):
     return number_of_estimators
 
 
+# saves the model using pickle
 def save_model(model, x_test, y_test, model_path, x_test_path, y_test_path):
     with open(model_path, "wb") as f:
         pickle.dump(model, f)
@@ -132,7 +159,7 @@ def save_model(model, x_test, y_test, model_path, x_test_path, y_test_path):
     y_test_txt.close()
 
 
-# receives x_test.iloc[i] as parameter, the input data for a single simulation
+# receives x_test.iloc[i] as parameter, the input data for a single simulation. Returns the input values as a string
 def format_x_test_string_data(x_test):
     formatted_string = "C: " + str(x_test.iat[0]) + " T1: " + str(x_test.iat[1]) + " T2: " + \
                        str(x_test.iat[2]) + " HDIST: " + str(x_test.iat[3])
@@ -140,6 +167,7 @@ def format_x_test_string_data(x_test):
     return formatted_string
 
 
+# splits the original csv to 2, input and output
 def split_csv(csv_data, csv_input, csv_output):
     lines = csv_data.readlines()
     line_number = 0
--- current_model.py
+++ current_model.py
@@ -16,7 +16,7 @@ data_output = pd.read_csv("Data/CSV/Currents/output_currents_half.csv", sep=",")
 # creating x by keeping only these columns from the input data
 x = data_input[["C", "T1", "T2", "HDIST"]]
 
-# creating y by keeping only the first 20 I(V6) values
+# creating y. Notice i/2 for the half step
 output_list = []
 for i in range(1, NUMBER_OF_CURRENT_VALUES+1):
     output_list.append("I(V6)-" + str(i/2))
@@ -25,13 +25,13 @@ y = data_output[output_list]
 # can be used to get the Primary Component Analysis of x, comparing the weights of its factors
 # common_tools.get_pca(x)
 
-n_estimators = 150
-
 # can be used to get the best hyperparameters
 # grid = {'estimator__n_estimators': [125, 150, 175]}   # it's 150
 # n_estimators = common_tools.grid_search(MultiOutputRegressor(RandomForestRegressor()), grid, x, y)
+n_estimators = 150
 
-# creating the model using a random forest regression algorithm wrapped in a multioutput module
+# creating the model using a random forest regression algorithm wrapped
+# in a multioutput module and using the number of trees found above
 model = MultiOutputRegressor(RandomForestRegressor(n_estimators=n_estimators))
 
 # split the dataset repeatedly in order to keep the model with the best (smallest) test error
@@ -40,9 +40,10 @@ average_test_error = 0
 for it in range(10):
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
 
+    # needed to set the input values in the plot
     x_test_values_for_plot_title = x_test
 
-    # keep the not standardized values in order to save the save the relevant scaler
+    # keeping the not standardized values in order to save the save the relevant scaler
     original_x_train = x_train
     original_x_test = x_test
 
@@ -51,11 +52,12 @@ for it in range(10):
 
     model.fit(x_train, y_train)
 
-    # get current test mean squared error but print train mse too
+    # get current test mean squared error. Print both train and test mse
     test_error = common_tools.train_test_mean_error("current", model, x_train, x_test, y_train, y_test)
 
     average_test_error += test_error
 
+    # keeping the best values to create the best model
     if best_test_error > test_error:
         best_test_error = test_error
         print("best error now is", best_test_error.round(2))
@@ -71,6 +73,8 @@ for it in range(10):
         break
 
 print("best error was", best_test_error.round(2), "average error was", (average_test_error/(it+1)).round(2))
+
+# using the best values found above
 x_train = best_x_train
 x_test = best_x_test
 y_train = best_y_train
@@ -79,15 +83,15 @@ x_test_values_for_plot_title = x_test_values_for_plot_title
 x_train_for_scaler = best_original_x_train
 x_test_for_scaler = best_original_x_test
 
-# save the scaler
+# save the scaler. 1 for current scaler
 common_tools.standardize_train_test_data(x_train_for_scaler, x_test_for_scaler, 1)
 
 # train the model using the best x_train and y_train
 model.fit(x_train, y_train)
 
 # save the model
-dump(model, open("Models/current_model.pkl", "wb"))
-exit()
+# dump(model, open("Models/current_model.pkl", "wb"))
+# exit()
 
 # use the model to predict the values y_test of the x_test
 test_prediction = model.predict(x_test)
